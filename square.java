0d38f613024e114c314343ccf39965c44079cb8a
shree5683 <134926115+shree5683@users.noreply.github.com>
2025-04-07 17:13:49 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ public class square{
+ public static void main(String x[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the side of square");
+ a=sc.nextInt();
+System.out.println("area of square"+a*a);
+
+
+} 
+}
